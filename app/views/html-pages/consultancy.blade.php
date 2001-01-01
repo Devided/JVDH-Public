d53151d2285f394e2095231a420a20c56d8d76a9
Duco Visbeen <duco@maximum.com>
2001-01-01 01:01:30 +0100

    t

--- app/views/html-pages/consultancy.blade.php
+++ app/views/html-pages/consultancy.blade.php
@@ -19,17 +19,40 @@
                                 Consultancy
                             </h2>
                             <p style="text-align: justify">
-                                Voor les kunnen kinderen meestal vanaf een jaar of 5/6 terecht bij een tennisvereniging. De allerkleinsten beginnen met de cursus "kidstennis". Hier maken de kinderen op de juiste manier kennis met het boeiende en veelzijdige spelletje tennis.<br>Met aangepast spelmateriaal wordt spelenderwijs het balgevoel, het anticipatie- en orientatievermogen verbeterd. Daarna stromen ze door naar de cursussen voor beginners en gevorderden.<br><br>Voor de groepslessen wordt er om praktische redenen op leeftijd en beschikbaarheid ingedeeld. Wij gaan het liefst zo veel mogelijk uit van het niveau en instelling van het kind.
+                                Een aantal maatschappelijke ontwikkelingen dwingt tennisverenigingen er toe ander club management te
+
+                                voeren. We constateren terugloop van het aantal georganiseerde sporters. Vergrijzing van de bevolking. Een
+
+                                meer individualistische levenswijze. Meer keuzes voor allerlei vormen van vrijetijdsbesteding. Als vereniging
+
+                                wil je wat bieden aan leden en potentiële leden. Hoe speel je in op ontwikkelingen binnen de vereniging en
+
+                                wat doe je er aan? Jeroen van den Heuvel kan als sporttechnischkader-adviseur de vereniging ondersteunen.<br><br>
+
+                                <b>Wat kunnen wij voor de vereniging betekenen</b><br>
+                                Als adviseur, van sporttechnischkader, zijn wij gepokt en gemazeld in het ondersteunen van verenigingen
+
+                                en commissies. Denk hierbij aan het ondersteunen bij het maken van jeugdbeleid, een trainingsplan of
+
+                                clubactiviteiten.<br><br>
+
+                                <b>Werkwijze</b><br>
+
+                            De werkwijze verschilt, afhankelijk van de wens van de vereniging. Het opzetten van een verenigings- of
+
+                            jeugdbeleidsplan vergt een andere aanpak dan het opzetten van een toernooi of een eendaagse clubactiviteit.<br><br>
+
+                            <b>Activiteiten</b><br>
+                            Behalve beleidsmatige zaken hebben wij veel ervaring in het organiseren en uitvoeren van verschillende
+
+                            club- en cluboverschrijdende activiteiten. Met als opleidingsachtergrond het CIOS Lichamelijke Opvoeding en
+
+                            met 20 jaar praktijk ervaring hebben wij genoeg kennis en kunde in huis om activiteiten op te zetten en te
+
+                            begeleiden.
                             </p>
 
-                            <p></p><br>
-                            <h2>Detachering</h2>
-                            <p style="text-align: justify">
-                                De tennistrainers van Tennisschool Jeroen van den Heuvel vormen een enthousiast team, dat zich kenmerkt door een deskundige en persoonsgebonden aanpak. Wij bieden tenniscursussen voor beginners, gevorderden en competitiespelers aan.
-                                <br><br>
-                                Naast het feit dat de beheersing van een goede techniek van de slagen meer speelplezier geeft vormt juist deze techniek de basis om blessure vrij te kunnen spelen.
-                            </p>
-                            <br></div>
+                            </div>
                     </div>
                     <div class="column_right" style="padding-top: 15px;">
                         <img src="{{ asset('img/jeroen.jpg') }}">
@@ -38,7 +61,46 @@
                         </div>
 
                     </div>
+                    <div class="column">
+                        <br>
+                        <h2>Detachering</h2>
+                        <p style="text-align: justify">
+                            Als vereniging wilt u dat alles perfect geregeld is. Geen omkijken naar de trainingen: u rekent
 
+                            op professionals, op betrokkenheid. TSJH werkt met enthousiaste trainers die het uiterste uit
+
+                            leerlingen willen halen. Of dat nu om het plezier of de prestatie gaat, het is de betrokkenheid
+
+                            van de trainers die het mogelijk maakt.<br><br>
+
+                            TSJH legt grote nadruk op het begeleiden, trainen en coachen van spelers op hun eigen
+
+                            niveau. Zo heeft zowel de recreatieve als de prestatieve speler maximaal profijt van de
+
+                            trainingen.<br><br>
+
+                            <b>Continuïteit gewaarborgd</b><br>
+                            Is uw trainer ziek? Dan staat er een andere TSJH trainer die precies weet met welk
+
+                            programma de groep bezig is. Het team van TSJH tennistrainers werkt met weekthema’s, één
+
+                            van de middelen waardoor de kwaliteit van de lessen gewaarborgd wordt en blijft.
+
+                            <br><br><b>Programma op maat</b><br>
+                            Naast deze professionele tennisdetachering biedt TSJH meer. Een programma op maat voor
+
+                            elke vereniging. Er wordt gekeken naar welke trainers het best ingezet kunnen worden en
+
+                            waar precies behoefte aan is. Zo wordt er gewerkt met vaste gezichten voor de vereniging.
+
+                            De trainers zijn professionals met passie die onder supervisie van eigenaar Jeroen van
+
+                            den Heuvel de training en coaching verzorgen. Geen zorgen meer over werkgeverschap,
+
+                            loonlijstconstructies of belastingen: TSJH tennistrainers zijn in loondienst bij TSJH.
+                        </p>
+                        <br>
+                        </div>
                 </div>
             </div>
         </div>
@@ -49,6 +111,7 @@
                 <div class="text noicon">
                     <h3>
                         Contact opnemen?</h3>
+                    Voor meer informatie of een oriënterend gesprek, kunt u contact op nemen met Jeroen van den Heuvel, middels het contactformulier
                     <a class="more blue icon_small_arrow margin_right_white" href="{{ action('contact') }}" style="margin-top:20px;" title="Details">Ga naar de contactpagina</a>
         </ul>
     </div>
